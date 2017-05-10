fb0edd7e269281dc9682015e9248a1c577456f5f
桃花林里练醉拳 <jsmrliu@gmail.com>
2017-05-10 17:51:26 +0800

    带进度下载apk

--- app/src/main/java/com/liusong/app/ui/activity/UpdateActivity.java
+++ app/src/main/java/com/liusong/app/ui/activity/UpdateActivity.java
@@ -1,16 +1,25 @@
 package com.liusong.app.ui.activity;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.liusong.app.R;
 import com.liusong.app.databinding.ActivityUpdateBinding;
 import com.liusong.library.updateapp.UpdateManager;
 import com.liusong.library.updateapp.UpdateUtils;
 import com.liusong.library.utils.ToastUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * apk dowloadManager下载更新
@@ -34,8 +43,11 @@ public class UpdateActivity extends AppCompatActivity implements View.OnClickLis
                 simpleUpdate();
                 break;
             case R.id.btn_progressbar_update:
-//                simpleUpdate();
-                mBinding.pbUpdate.setProgress(15);
+                simpleUpdate();
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+                long downId = sp.getLong(DownloadManager.EXTRA_DOWNLOAD_ID, -1L);
+                updateViews(mBinding.pbUpdate,downId);
+//                mBinding.pbUpdate.setProgress(15);
                 break;
         }
     }
@@ -50,5 +62,43 @@ public class UpdateActivity extends AppCompatActivity implements View.OnClickLis
         }else{
             ToastUtils.showToast(this,"已是最新，无需下载！");
         }
+    }
+
+    /**
+     * 进度条更新
+     * @param downlaodId
+     */
+    private void updateViews(final ProgressBar progressBar,final long downlaodId) {
+        final Timer myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                DownloadManager downloadManager=(DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                DownloadManager.Query query = new DownloadManager.Query().setFilterById(downlaodId);
+                Cursor cursor = downloadManager.query(query);
+                cursor.moveToFirst();
+                int curProgress = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                int totalProgress = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+                cursor.close();
+                final int progress = (curProgress * 100 /totalProgress);
+                if (progress == 100) {
+                    myTimer.cancel();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progress);
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progress);
+                        }
+                    });
+                }
+            }
+        }, 0, 1);
+
     }
 }
