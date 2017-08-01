710239d09abb07e9cd5203c8db0a5b3a1032e0bd
Jason <cnmrliu@163.com>
2017-08-01 16:03:10 +0800

    third party: jpush

--- app/src/main/java/com/liusong/app/ui/thirdparty/JPushActivity.java
+++ app/src/main/java/com/liusong/app/ui/thirdparty/JPushActivity.java
@@ -27,10 +27,9 @@ public class JPushActivity extends BaseActivity {
         String[] permissions = {
                 Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+//                Manifest.permission.ACCESS_FINE_LOCATION,
+//                Manifest.permission.ACCESS_COARSE_LOCATION,
         };
         if (!hasPermission(permissions)) {
             requestPermission(Constants.GRANT_ALL_CODE, getUnGrantPermissions(permissions));
