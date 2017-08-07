a6dca96ac997bf640ec193b2f39bc09cd884e5a5
Jason <cnmrliu@163.com>
2017-08-07 15:15:40 +0800

    进程包活

--- app/src/main/java/com/liusong/app/service/LiveService.java
+++ app/src/main/java/com/liusong/app/service/LiveService.java
@@ -13,6 +13,11 @@ import android.support.annotation.Nullable;
  */
 public class LiveService extends Service {
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
